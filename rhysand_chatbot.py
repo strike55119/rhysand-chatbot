bc93f2696c611bdaa157e096f6bd6cf7fb9f4906
strike55119 <peterrip2@yahoo.com>
2025-06-29 21:36:32 -0500

    Update rhysand_chatbot.py

--- rhysand_chatbot.py
+++ rhysand_chatbot.py
@@ -87,4 +87,5 @@ def chat():
     return render_template_string(HTML_TEMPLATE, messages=chat_history)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
