bc6b953de4819e9aede393134768fa49d39e0cfc
strike55119 <peterrip2@yahoo.com>
2025-06-30 11:27:27 -0500

    Update rhysand_chatbot.py

--- rhysand_chatbot.py
+++ rhysand_chatbot.py
@@ -68,5 +68,6 @@ def chat():
 
 #-------------------------------------------------------------
 
-if name == 'main': port = int(os.environ.get('PORT', 5000)) app.run(host='0.0.0.0', port=port)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
